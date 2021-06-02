81348a9d7b8048741db294b5107594fc6ca813ad
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-02 20:15:17 -0300

    Added cookie-session middleware and validated the required ENV variables

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
@@ -11,7 +12,14 @@ import { signupRouter } from './routes/signup';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +33,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY key must be defined!');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
             useNewUrlParser: true,
@@ -32,7 +44,7 @@ const start = async () => {
             useUnifiedTopology: true,
         });
 
-        console.log('DB connected')
+        console.log('DB connected');
     } catch (error) {
         console.log(error);
     }
@@ -42,4 +54,4 @@ const start = async () => {
     });
 };
 
-start()
+start();
