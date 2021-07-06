1f84bca498d567b36ced02fe2b0bef4162a0e1ca
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-06 14:09:47 -0300

    Connect to nats streaming server

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -8,15 +9,36 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI key must be defined!');
     }
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID key must be defined!');
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID key must be defined!');
+    }
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL key must be defined!');
+    }
 
     try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
+        natsWrapper.client.on('close', () => {
+            console.log('Closing NATS connection');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
         });
+        console.log('MongoDB connected');
 
-        console.log('DB connected');
     } catch (error) {
         console.log(error);
     }
