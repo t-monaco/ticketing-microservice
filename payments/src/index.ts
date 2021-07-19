c7d26b3fe81438708ae653606a3998fea94d6328
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-18 23:39:54 -0300

    Added router

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -33,6 +35,8 @@ const start = async () => {
         process.on('SIGTERM', () => natsWrapper.client.close());
 
         // --- Listeners ---
+        new OrderCreatedListener(natsWrapper.client).listen();
+        new OrderCancelledListener(natsWrapper.client).listen();
         // ---
 
         await mongoose.connect(process.env.MONGO_URI, {
