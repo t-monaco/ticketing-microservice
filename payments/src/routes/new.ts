2d7e1865a0eba5a13eef55273b964dbe73fa32e4
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-19 11:21:09 -0300

    Managed payments

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,7 +8,11 @@ import {
 } from '@tm-ticketing/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
+import { natsWrapper } from '../nats-wrapper';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -34,7 +38,26 @@ router.post(
             throw new BadRequestError('Opps... oprder expire!');
         }
 
-        res.send({ success: true });
+        const charge = await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token,
+        });
+
+        const payment = Payment.build({
+            orderId,
+            stripeId: charge.id,
+        });
+
+        await payment.save();
+
+        new PaymentCreatedPublisher(natsWrapper.client).publish({
+            id: payment.id,
+            orderId: payment.orderId,
+            stripeId: payment.stripeId,
+        });
+
+        res.status(201).send({ payment });
     }
 );
 
