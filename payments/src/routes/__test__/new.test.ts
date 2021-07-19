780d8bcf41998dff95d907feb944da7dfddb43e3
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-19 11:20:04 -0300

    Updated test to use real STRIPE API

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { Payment } from '../../models/payment';
+import { stripe } from '../../stripe';
 
 it('returns a 404, when purchasing an orther that does not exits', async () => {
     await request(app)
@@ -59,4 +61,45 @@ it('returns a 400, when purchasing a cancelled order', async () => {
             orderId: order.id,
         })
         .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+    const price = Math.floor(Math.random() * 100000);
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        userId: userId,
+        price,
+        status: OrderStatus.Created,
+    });
+
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signup(userId))
+        .send({
+            token: 'tok_visa',
+            orderId: order.id,
+        })
+        .expect(201);
+
+    const stripeCharges = await stripe.charges.list({ limit: 50 });
+
+    const stripeCharge = stripeCharges.data.find((charge) => {
+        return charge.amount === price * 100;
+    });
+
+    const payment = await Payment.findOne({
+        orderId: order.id,
+        stripeId: stripeCharge!.id
+    })
+
+    expect(stripeCharge).toBeDefined();
+    expect(stripeCharge!.amount).toEqual(price * 100);
+    expect(stripeCharge!.currency).toEqual('usd');
+
+    expect(payment).not.toBeNull()
 });
