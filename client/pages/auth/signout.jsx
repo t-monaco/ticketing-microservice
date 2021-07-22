9864b41dff29b7eb30900a1c5010fa79096460cf
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-22 08:34:31 -0300

    Changed file extension to jsx

--- client/pages/auth/signout.jsx
+++ client/pages/auth/signout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Router from 'next/router';
-import useRequest from './../../hooks/use-request';
+import useRequest from '../../hooks/use-request';
 
 const SignOut = () => {
     const {doRequest} = useRequest({
