f9b9e57c8f24d87a87e703c63eb9d606dd4edbcb
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-24 22:16:24 -0300

    Woking on new look

--- client/components/header/header.component.jsx
+++ client/components/header/header.component.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+import breakpoint from '../../_breakpoints';
 
 const Header = ({ currentUser }) => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
     const links = [
         !currentUser && { label: 'Sign Up', href: '/auth/signup' },
         !currentUser && { label: 'Sign In', href: '/auth/signin' },
@@ -12,23 +16,113 @@ const Header = ({ currentUser }) => {
         .filter((linkConfig) => linkConfig)
         .map(({ label, href }, key) => {
             return (
-                <li key={key} className='nav-item'>
+                <ListItem key={key} className='nav-item'>
                     <Link href={href}>
                         <a className='nav-link'>{label}</a>
                     </Link>
-                </li>
+                </ListItem>
             );
         });
     return (
-        <nav className='navbar navbar-dark bg-dark'>
-            <Link href='/'>
-                <a className='navbar-brand'>LOGO</a>
-            </Link>
-            <div className='d-flex justify-content-end'>
-                <ul className='nav d-flex align-items-center'>{links}</ul>
-            </div>
-        </nav>
+        <HeaderContainer>
+            <NavContainer>
+                <Link href='/'>
+                    <a>LOGO</a>
+                </Link>
+                <LinksContainer
+                    className={`${showSidebar ? 'show-sidebar' : ''}`}
+                >
+                    <LinksList>{links}</LinksList>
+                </LinksContainer>
+                <ToggleMenu onClick={() => setShowSidebar(!showSidebar)}>
+                    CLICK
+                </ToggleMenu>
+            </NavContainer>
+        </HeaderContainer>
     );
 };
 
 export default Header;
+
+const HeaderContainer = styled.div`
+    background-color: red;
+    height: 60px;
+    width: 100vw;
+`;
+
+const NavContainer = styled.nav`
+    padding: 1em;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+`;
+const LinksContainer = styled.div`
+    background-color: lightgoldenrodyellow;
+    padding: 2em;
+    position: absolute;
+    top: 60px;
+    right: 0;
+    height: calc(100vh - 60px);
+    width: 60%;
+    transform: translateX(100%);
+    transition: transform 0.3s ease-in-out;
+
+    &.show-sidebar {
+        transform: translateX(0);
+    }
+
+    @media screen and (${breakpoint.device.sm}) {
+        background-color: blueviolet;
+        position: relative;
+        padding: 0;
+        top: unset;
+        right: unset;
+        height: unset;
+        display: grid;
+        place-items: center;
+        transform: translateX(0);
+    }
+`;
+
+const LinksList = styled.ul`
+    background-color: greenyellow;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 0.5em;
+    width: 100%;
+
+    @media screen and (${breakpoint.device.sm}) {
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: 1fr;
+        grid-auto-columns: auto;
+    }
+`;
+
+const ListItem = styled.li`
+    width: 100%;
+
+    a {
+        background-color: blueviolet;
+        display: block;
+        padding: 0.5em 1em;
+        text-align: center;
+        width: 100%;
+    }
+
+    @media screen and (${breakpoint.device.sm}) {
+        a {
+            padding: 0;
+        }
+    }
+`;
+
+const ToggleMenu = styled.div`
+    cursor: pointer;
+    display: block;
+
+    @media screen and (${breakpoint.device.sm}) {
+        display: none;
+    }
+`;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Header from '../components/header/header.component';
 import buildClient from '../api/build-client';
+import Head from 'next/head'
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;800&display=swap" rel="stylesheet" />
+            </Head>
+            <GlobalStyle />
             <Header currentUser={currentUser} />
             <div className='container'>
                 <Component currentUser={currentUser} {...pageProps} />
@@ -33,3 +41,20 @@ AppComponent.getInitialProps = async (appContext) => {
 };
 
 export default AppComponent;
+
+const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    font-family: 'Nunito', sans-serif;
+    margin: 0 auto;
+    padding: 0;
+}
+
+li {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+`;
